2c9555241959b2e5383c059440a3564658cc55ec
Bartman2602 <bartoszgut@wp.pl>
2020-04-04 09:32:00 +0200

    Create fizyka icy tower.cpp

---
+++ fizyka icy tower.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<cstdio>
+#include<windows.h>
+using namespace std;
+
+class gravity{
+	public:
+		gravity(int boardWidth){
+			this->boardWidth=boardWidth;
+			
+		}
+		void startJump(){
+			speedY=-5;
+			jumping=true;
+		}
+		
+		bool isJumping(){
+			
+			return jumping;
+		}
+		void nextStep(){
+			speedY+=step;
+			
+		}
+		int getChangeX(){
+			
+			return speedX;
+		}
+		int getChangeY(){
+		
+			return speedY;
+		}
+		void leftPress(int actPlayerX){
+			speedX=1;
+			if(actPlayeX==0)
+				speedX=0;
+			if(speedX<3)
+				speedX+=step;
+			
+		}
+		void rightPress(int actPlayerX){
+			speedX=1;
+			if(actPlayeX==boardWidth)
+				speedX=0;
+			if(speedX<3)
+				speedX+=step;
+			
+		
+		}
+		
+	private;
+		double speedX;
+		double speedY;
+		bool jumping=false;
+		double step=0.2;
+			
+};
+main(){
+	
+	
+	
+	return 0;
+}
