83d1a590714839599dbbac17892587cabae0a042
KonradToNieJa <ktekieli05@gmail.com>
2020-04-04 09:30:28 +0200

    Create IcyTower.cpp

---
+++ IcyTower.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<windows.h>
+#include<conio.h>
+using namespace std;
+
+
+    class Player{
+        public:
+            Player(Gravity gravity){
+                this.gravity=gravity
+            }
+            void playerStep(){
+                if(GetAsyncState(VK_LEFT)){
+                    gravity.leftPress();
+
+                }
+                if(GetAsyncState(VK_RIGHT)){
+                    gravity.rightPress();
+
+                    }
+                if(GetAsyncState(VK_SPACE)&&gravity.isJumping==false){
+                    gravity.startJump();
+
+                }
+            x+=gravity.ChangeX();
+            gravity.nextStep();
+            y+=gravity.ChangeY();
+            }
+
+
+        private:
+            double x,y;
+
+
+    };
+    class Points{
+        public:
+
+
+    };
